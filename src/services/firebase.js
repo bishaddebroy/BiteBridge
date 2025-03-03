78f405249021afc1b6d1f478db70404fb43162b7
Bishad Deb Roy Utsha <royud11@gmail.com>
2025-03-02 20:40:40 -0400

    gitignore updated

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,16 +3,7 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// Your Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBPVxETUBsHuKvMDBIlJs83xuFThW2WvrE",
-    authDomain: "bitebridge-47816.firebaseapp.com",
-    projectId: "bitebridge-47816",
-    storageBucket: "bitebridge-47816.firebasestorage.app",
-    messagingSenderId: "784765858080",
-    appId: "1:784765858080:web:08ce97f1c6c39fc9d1e745"
-  };
+import { firebaseConfig } from './firebaseConfig';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
